96e53ad29bff5274689223c6090555eda151c507
nobody-27 <nobodypal27@gmail.com>
2023-03-27 13:03:53 +0530

    update the url

--- assigment/vehicle/urls.py
+++ assigment/vehicle/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('add-vehicle/',views.Add_Vehicle.as_view(),name="add_vehicle"),
     path('edit-vehicle/',views.Edit_vehicle.as_view(),name="edit_vehicle"),
 
-    path('get-vehicle-details/<int:id>/',views.get_vehicle_details.as_view(),name="get_vehicle_details"),
+    path('get-vehicle-details/<int:id>/',login_required(views.get_vehicle_details.as_view()),name="get_vehicle_details"),
     path('delete-vehicle/<int:id>/',views.Delete_Vehicle.as_view(),name="delete_Vehicle")
 
     
